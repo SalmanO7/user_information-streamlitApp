e3865c1fe4920bc703e030f36c2b0608046f05e9
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2025-03-06 02:31:16 +0500

    complete show user information streamlit app

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('Welcome to my first Streamlit app')
+st.write('This is a simple app to show users information')
+
+st.header('Input some data')
+name = st.text_input('Enter your name')
+age = st.number_input('Enter your age', min_value=0, step=1)
+
+# Submit Button
+if st.button("Submit"):
+    if not name or not age:
+        st.warning('Please input some data')        
+    else:
+        st.success(f'Hello, I am {name} and I am {age} years old 🎉')
