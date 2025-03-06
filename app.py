873db428b60b45384db2bddebe39eb2a612c3e7a
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2025-03-07 01:30:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ st.header('Input some data')
 name = st.text_input('Enter your name')
 age = st.number_input('Enter your age', min_value=0, step=1)
 
-# Submit Button
 if st.button("Submit"):
     if not name or not age:
         st.warning('Please input some data')        
